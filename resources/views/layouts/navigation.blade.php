0d19449997db37a097d4878d77eec158a6002340
bastin1945 <bastin354@gmail.com>
2024-12-23 07:25:13 +0700

    navv

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -31,9 +31,34 @@
         </div>
 
         <!-- Profile -->
-        <div class="flex items-center space-x-3">
-            <img src="{{ asset('assets/user.png') }}" alt="Profile" class="h-10 w-10 rounded-full">
-            <span class="text-white font-medium">Hi, {{ Auth::user()->name }}</span>
+        <div class="relative flex items-center space-x-3">
+            <!-- Profile Icon -->
+            <img src="{{ asset('assets/user.png') }}" alt="Profile" class="h-10 w-10 rounded-full cursor-pointer" @click="open = !open">
+            <span class="text-white font-medium cursor-pointer" @click="open = !open">Hi, {{ Auth::user()->name }}</span>
+
+            <!-- Dropdown Menu -->
+            <div 
+                x-show="open" 
+                @click.away="open = false" 
+                x-transition:enter="transition ease-out duration-200" 
+                x-transition:enter-start="opacity-0 transform scale-95" 
+                x-transition:enter-end="opacity-100 transform scale-100" 
+                x-transition:leave="transition ease-in duration-150" 
+                x-transition:leave-start="opacity-100 transform scale-100" 
+                x-transition:leave-end="opacity-0 transform scale-95" 
+                class="absolute right-0 mt-2 w-56 bg-white border border-gray-200 rounded-lg shadow-xl z-50"
+            >
+                <a href="{{ route('profile.edit') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-50 hover:text-blue-600">
+                    {{ __('Profile') }}
+                </a>
+                <hr class="border-gray-200 my-1">
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button type="submit" class="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-red-50 hover:text-red-600">
+                        {{ __('Log Out') }}
+                    </button>
+                </form>
+            </div>
         </div>
     </div>
 </nav>
