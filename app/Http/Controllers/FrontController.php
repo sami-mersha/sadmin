b2443d2c384d38f32d4f534ccc2481b058e8cfde
fathtrro <fatihaziz222@gmail.com>
2025-01-23 00:18:13 +0700

    sjdhjshdjhjshdj

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,16 +2,74 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Konser;
+use App\Models\konser;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     */
     public function index()
     {
-        $konsers = Konser::with('tiket')->paginate('6');
-        
+        $konsers = konser::whereHas('tiket', function ($query) {
+            $query->where('jenis_tiket', 'Regular');
+        })->with([
+                    'tiket' => function ($query) {
+                        $query->where('jenis_tiket', 'Regular');
+                    }
+                ])->get();
+
+
         // dd($konsers->toArray());
         return view('dashboard', compact('konsers'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return view('detailkonser');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
